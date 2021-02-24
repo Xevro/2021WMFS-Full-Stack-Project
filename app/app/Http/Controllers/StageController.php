30402adb9665eac42eaf231f061f6bfb792e5fe4
Louis D'Hont <louisdhont@MacBook-Pro-15-van-Louis.local>
2021-02-24 13:00:50 +0100

    creating StageController

---
+++ app/app/Http/Controllers/StageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Proposals;
+
+class StageController extends Controller
+{
+    //
+
+    public function overview() {
+       // dd(Proposals::find(1)->brand->name);
+    }
+}
