93d62384d2bd5a4ac9b9cb189bc9cef62a7a1a3a
Louis D'Hont <louisdhont@MacBook-Pro-15-van-Louis.local>
2021-03-04 13:08:57 +0100

    Adding prefix for dashboard route

--- app/routes/web.php
+++ app/routes/web.php
@@ -17,26 +17,29 @@ Route::get('/', function () {
     return redirect('/dashboard');
 });
 
-Route::get('/dashboard', [StageController::class, 'overview']);
+Route::prefix('dashboard')->group(function(){
+    Route::get('/', [StageController::class, 'overview']);
 
-Route::get('/dashboard/students', [StageController::class, 'students']);
-Route::get('/dashboard/student/{id}', [StageController::class, 'studentDetail'])->where(['id' => '[0-9]+']);
+    Route::get('/students', [StageController::class, 'students']);
+    Route::get('/student/{id}', [StageController::class, 'studentDetail'])->where(['id' => '[0-9]+']);
 
-Route::get('/dashboard/companies', [StageController::class, 'companies']);
-Route::get('/dashboard/company/{id}', [StageController::class, 'companyDetail'])->where(['id' => '[0-9]+']);
+    Route::get('/companies', [StageController::class, 'companies']);
+    Route::get('/company/{id}', [StageController::class, 'companyDetail'])->where(['id' => '[0-9]+']);
 
-Route::get('/dashboard/company/proposal/{id}', [StageController::class, 'proposalDetail'])->where(['id' => '[0-9]+']);
+    Route::get('/company/proposal/{id}', [StageController::class, 'proposalDetail'])->where(['id' => '[0-9]+']);
 
-//add company
-Route::get('/dashboard/company/add', [StageController::class, 'showAddCompany']);
-Route::post('/dashboard/company/add', [StageController::class, 'addCompany']);
-//add student
-Route::get('/dashboard/student/add', [StageController::class, 'showAddStudent']);
-Route::post('/dashboard/student/add', [StageController::class, 'addStudent']);
-//add student to proposal
-Route::get('/dashboard/company/proposal/{id}/assign', [StageController::class, 'showAssignStudentToProposal'])->where(['id' => '[0-9]+']);
-Route::post('/dashboard/company/proposal/{id}/assign', [StageController::class, 'assignStudentToProposal'])->where(['id' => '[0-9]+']);
-//Accept proposal on other page
-Route::get('/dashboard/company/proposal/{id}/validate', [StageController::class, 'showValidateProposal'])->where(['id' => '[0-9]+']);
-Route::post('/dashboard/company/proposal/{id}/validate', [StageController::class, 'validateProposal'])->where(['id' => '[0-9]+']);
+    //add company
+    Route::get('/company/add', [StageController::class, 'showAddCompany']);
+    Route::post('/company/add', [StageController::class, 'addCompany']);
+    //add student
+    Route::get('/student/add', [StageController::class, 'showAddStudent']);
+    Route::post('/student/add', [StageController::class, 'addStudent']);
+    //add student to proposal
+    Route::get('/company/proposal/{id}/assign', [StageController::class, 'showAssignStudentToProposal'])->where(['id' => '[0-9]+']);
+    Route::post('/company/proposal/{id}/assign', [StageController::class, 'assignStudentToProposal'])->where(['id' => '[0-9]+']);
+    //Accept proposal on other page
+    Route::get('/company/proposal/{id}/validate', [StageController::class, 'showValidateProposal'])->where(['id' => '[0-9]+']);
+    Route::post('/company/proposal/{id}/validate', [StageController::class, 'validateProposal'])->where(['id' => '[0-9]+']);
 
+
+});
