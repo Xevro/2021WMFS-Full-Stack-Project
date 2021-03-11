111cbe5369f45bf86ec4083de9d14f8f0e56bee4
Louis D'Hont <29582386+Xevro@users.noreply.github.com>
2021-03-11 22:44:45 +0100

    installing breeze & tailwind and installing npm for login form

--- app/resources/views/dashboard.blade.php
+++ app/resources/views/dashboard.blade.php
@@ -6,58 +6,58 @@
     <div class="horizontal-main-wrapper">
         <!-- main content area start -->
     @yield('navbar')
-            <!--  Begin amounts -->
-            <div class="col-lg-8">
-                <div class="row">
-                    <div class="col-md-6 mt-5 mb-3">
-                        <div class="card">
-                            <div class="seo-fact sbg2">
-                                <div class="p-4 d-flex justify-content-between align-items-center">
-                                    <div class="seofct-icon"><i class="ti-thumb-up"></i>Goedgekeurde voorstellen</div>
-                                    <h2>{{ $amountApproved }}</h2>
-                                </div>
+    <!--  Begin amounts -->
+        <div class="col-lg-8">
+            <div class="row">
+                <div class="col-md-6 mt-5 mb-3">
+                    <div class="card">
+                        <div class="seo-fact sbg2">
+                            <div class="p-4 d-flex justify-content-between align-items-center">
+                                <div class="seofct-icon"><i class="ti-thumb-up"></i>Goedgekeurde voorstellen</div>
+                                <h2>{{ $amountApproved }}</h2>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6 mt-md-5 mb-3">
-                        <div class="card">
-                            <div class="seo-fact sbg1">
-                                <div class="p-4 d-flex justify-content-between align-items-center">
-                                    <div class="seofct-icon"><i class="ti-filter"></i> Aantal goed te keuren voorstellen</div>
-                                    <h2>{{ $amountToCheck }}</h2>
-                                </div>
+                </div>
+                <div class="col-md-6 mt-md-5 mb-3">
+                    <div class="card">
+                        <div class="seo-fact sbg1">
+                            <div class="p-4 d-flex justify-content-between align-items-center">
+                                <div class="seofct-icon"><i class="ti-filter"></i> Aantal goed te keuren voorstellen</div>
+                                <h2>{{ $amountToCheck }}</h2>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- End amount -->
-            <!-- Progress Table start -->
-            <div class="col-12 mt-5">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="search-box pull-right pb-3">
-                            <form method="get" action="{{ url('/dashboard') }}">
-                                    <div class="row">
-                                        <div class="pr-3 pt-2">
-                                            <div class="custom-control custom-checkbox">
-                                                <input type="checkbox" @if ($status == 'on') checked @endif class="custom-control-input" name="status" id="status">
-                                                <label class="custom-control-label" for="status">Status Goedgekeurd?</label>
-                                            </div>
-                                        </div>
-                                        <div class="pr-4">
-                                            <input type="text" class="form-control" name="search" id="search" value="{{ $term ?? '' }}" placeholder="Zoekterm"/>
-                                        </div>
-                                        <div class="pr-3">
-                                            <button type="submit" class="btn btn-primary">Zoek</button>
-                                        </div>
+        </div>
+        <!-- End amount -->
+        <!-- Progress Table start -->
+        <div class="col-12 mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <div class="search-box pull-right pb-3">
+                        <form method="get" action="{{ url('/dashboard') }}">
+                            <div class="row">
+                                <div class="pr-3 pt-2">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" @if ($status == 'on') checked @endif class="custom-control-input" name="status" id="status">
+                                        <label class="custom-control-label" for="status">Status Goedgekeurd?</label>
                                     </div>
-                            </form>
-                        </div>
-                        <h4 class="header-title">Stage voorstellen overzicht</h4>
-                        <div class="single-table">
-                            <div class="table-responsive">
-                                @if($proposals->count() > 0)
+                                </div>
+                                <div class="pr-4">
+                                    <input type="text" class="form-control" name="search" id="search" value="{{ $term ?? '' }}" placeholder="Zoekterm"/>
+                                </div>
+                                <div class="pr-3">
+                                    <button type="submit" class="btn btn-primary">Zoek</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <h4 class="header-title">Stage voorstellen overzicht</h4>
+                    <div class="single-table">
+                        <div class="table-responsive">
+                            @if($proposals->count() > 0)
                                 <table class="table table-hover progress-table text-center">
                                     <thead class="text-uppercase">
                                     <tr>
@@ -73,7 +73,9 @@
                                     <tbody>
                                     @foreach($proposals as $proposal)
                                         <tr>
-                                            <td><a href="{{ url('/dashboard/company/' . $proposal->company->id) }}">{{ $proposal->company->name }}</a></td>
+                                            <td>
+                                                <a href="{{ url('/dashboard/company/' . $proposal->company->id) }}">{{ $proposal->company->name }}</a>
+                                            </td>
                                             <td>{{ $proposal->created_at }}</td>
                                             <td>{{ $proposal->start_period }}</td>
                                             <td>{{ $proposal->end_period }}</td>
@@ -96,40 +98,32 @@
                                 <div class="p-3 pull-right">
                                     {{ $proposals->links() }}
                                 </div>
-                                @else
+                            @else
                                 <p>Geen resultaten gevonden die {{ $term }} bevatte.</p>
-                                @endif
-                            </div>
+                            @endif
                         </div>
                     </div>
                 </div>
             </div>
             <!-- Progress Table end -->
         </div>
-            <!-- page title area end -->
-            <div class="main-content-inner">
-                <!-- sales report area start -->
-                <div class="sales-report-area mt-5 mb-5">
-                    <!-- table here -->
-                </div>
-                <!-- sales report area end -->
-                <!-- overview area start -->
-                <div class="row">
-                    <div class="col-xl-9 col-lg-8">
-
-                    </div>
-                    <div class="col-xl-3 col-lg-4 coin-distribution">
-
-                    </div>
-                </div>
+        <!-- page title area end -->
+        <div class="main-content-inner">
+            <!-- sales report area start -->
+            <div class="sales-report-area mt-5 mb-5">
+                <!-- table here -->
             </div>
-        <!-- main content area end -->
-        <!-- footer area start-->
-        <footer>
-            <div class="footer-area">
-                <p>© Copyright 2021. All right reserved. <a href="https://odisee.be/">Odisee</a> Technologiecampus.</p>
-            </div>
-        </footer>
-        <!-- footer area end-->
-    <!-- page container area end -->
+            <!-- sales report area end -->
+            <!-- overview area start -->
+            <div class="row">
+                <div class="col-xl-9 col-lg-8">
+                    <!-- main content area end -->
+                    <!-- footer area start-->
+                    <footer>
+                        <div class="footer-area">
+                            <p>© Copyright 2021. All right reserved. <a href="https://odisee.be/">Odisee</a>Technologiecampus.</p>
+                        </div>
+                    </footer>
+                    <!-- footer area end-->
+                    <!-- page container area end -->
 @endsection
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StageController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,42 +14,49 @@ use App\Http\Controllers\StageController;
 |
 */
 
+
+/*Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+*/
 Route::get('/', function () {
     return redirect('/dashboard');
 });
 
 Route::prefix('dashboard')->group(function () {
-    Route::get('/', [StageController::class, 'overview']);
+    Route::get('/', [StageController::class, 'overview'])->middleware(['auth']);
 
-    Route::get('/students', [StageController::class, 'students']);
-    Route::get('/student/{id}', [StageController::class, 'studentDetail'])->where(['id' => '[0-9]+']);
+    Route::get('/students', [StageController::class, 'students'])->middleware(['auth']);
+    Route::get('/student/{id}', [StageController::class, 'studentDetail'])->where(['id' => '[0-9]+'])->middleware(['auth']);
 
-    Route::get('/companies', [StageController::class, 'companies']);
-    Route::get('/company/{id}', [StageController::class, 'companyDetail'])->where(['id' => '[0-9]+']);
+    Route::get('/companies', [StageController::class, 'companies'])->middleware(['auth']);
+    Route::get('/company/{id}', [StageController::class, 'companyDetail'])->where(['id' => '[0-9]+'])->middleware(['auth']);
 
-    Route::get('/company/proposal/{id}', [StageController::class, 'proposalDetail'])->where(['id' => '[0-9]+']);
+    Route::get('/company/proposal/{id}', [StageController::class, 'proposalDetail'])->where(['id' => '[0-9]+'])->middleware(['auth']);
 
     //add company
-    Route::get('/company/add', [StageController::class, 'showAddCompany']);
-    Route::post('/company/add', [StageController::class, 'addCompany']);
+    Route::get('/company/add', [StageController::class, 'showAddCompany'])->middleware(['auth']);
+    Route::post('/company/add', [StageController::class, 'addCompany'])->middleware(['auth']);
 
     //add proposal
-    Route::get('/proposal/add', [StageController::class, 'showAddProposal']);
-    Route::post('/proposal/add', [StageController::class, 'addProposal']);
+    Route::get('/proposal/add', [StageController::class, 'showAddProposal'])->middleware(['auth']);
+    Route::post('/proposal/add', [StageController::class, 'addProposal'])->middleware(['auth']);
 
     //Delete proposal
-    Route::get('/company/proposal/{id}/delete', [StageController::class, 'showProposalDelete'])->where(['id' => '[0-9]+']);
-    Route::post('/company/proposal/{id}/delete', [StageController::class, 'proposalDelete'])->where(['id' => '[0-9]+']);
+    Route::get('/company/proposal/{id}/delete', [StageController::class, 'showProposalDelete'])->where(['id' => '[0-9]+'])->middleware(['auth']);
+    Route::post('/company/proposal/{id}/delete', [StageController::class, 'proposalDelete'])->where(['id' => '[0-9]+'])->middleware(['auth']);
 
     //add student
-    Route::get('/student/add', [StageController::class, 'showAddStudent']);
-    Route::post('/student/add', [StageController::class, 'addStudent']);
+    Route::get('/student/add', [StageController::class, 'showAddStudent'])->middleware(['auth']);
+    Route::post('/student/add', [StageController::class, 'addStudent'])->middleware(['auth']);
 
     //add student to proposal
-    Route::get('/company/proposal/{id}/assign', [StageController::class, 'showAssignStudentToProposal'])->where(['id' => '[0-9]+']);
-    Route::post('/company/proposal/{id}/assign', [StageController::class, 'assignStudentToProposal'])->where(['id' => '[0-9]+']);
+    Route::get('/company/proposal/{id}/assign', [StageController::class, 'showAssignStudentToProposal'])->where(['id' => '[0-9]+'])->middleware(['auth']);
+    Route::post('/company/proposal/{id}/assign', [StageController::class, 'assignStudentToProposal'])->where(['id' => '[0-9]+'])->middleware(['auth']);
 
     //Accept proposal on other page
-    Route::get('/company/proposal/{id}/validate', [StageController::class, 'showValidateProposal'])->where(['id' => '[0-9]+']);
-    Route::post('/company/proposal/{id}/validate', [StageController::class, 'validateProposal'])->where(['id' => '[0-9]+']);
+    Route::get('/company/proposal/{id}/validate', [StageController::class, 'showValidateProposal'])->where(['id' => '[0-9]+'])->middleware(['auth']);
+    Route::post('/company/proposal/{id}/validate', [StageController::class, 'validateProposal'])->where(['id' => '[0-9]+'])->middleware(['auth']);
 });
+
+require __DIR__ . '/auth.php';
