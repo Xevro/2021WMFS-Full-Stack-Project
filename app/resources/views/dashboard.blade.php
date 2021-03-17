16f7795c71105d33c04fa7ee66f092733c63055c
Xevro <29582386+Xevro@users.noreply.github.com>
2021-03-17 20:05:40 +0100

    adding list of proposals to mentor page

--- app/resources/views/dashboard.blade.php
+++ app/resources/views/dashboard.blade.php
@@ -67,7 +67,7 @@
                                             <th scope="col">Start datum</th>
                                             <th scope="col">End datum</th>
                                             <th scope="col">Beschrijving</th>
-                                            <th scope="col">status</th>
+                                            <th scope="col">status voorstel</th>
                                             <th scope="col">Actie</th>
                                         </tr>
                                         </thead>
@@ -123,7 +123,7 @@
                                             <th scope="col">Start datum</th>
                                             <th scope="col">End datum</th>
                                             <th scope="col">Beschrijving</th>
-                                            <th scope="col">status</th>
+                                            <th scope="col">status voorstel</th>
                                             <th scope="col">Actie</th>
                                         </tr>
                                         </thead>
@@ -234,6 +234,77 @@
                             </div>
                         </div>
                         <!-- Progress Table end -->
+                        <!-- Progress Table start -->
+                        <div class="col-12 mt-5">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="search-box pull-right pb-3">
+                                        <form method="get" action="{{ url('/dashboard') }}">
+                                            <div class="row">
+                                                <div class="pr-3 pt-2">
+                                                    <div class="custom-control custom-checkbox">
+                                                        <input type="checkbox" @if ($status == 'on') checked @endif class="custom-control-input" name="status" id="status">
+                                                        <label class="custom-control-label" for="status">Status Goedgekeurd?</label>
+                                                    </div>
+                                                </div>
+                                                <div class="pr-4">
+                                                    <input type="text" class="form-control" name="search" id="search" value="{{ $term ?? '' }}" placeholder="Zoekterm"/>
+                                                </div>
+                                                <div class="pr-3">
+                                                    <button type="submit" class="btn btn-primary">Zoek</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <h4 class="header-title">Stage voorstellen overzicht</h4>
+                                    <div class="single-table">
+                                        <div class="table-responsive">
+                                            @if($proposals->count() > 0)
+                                                <table class="table table-hover progress-table text-center">
+                                                    <thead class="text-uppercase">
+                                                    <tr>
+                                                        <th scope="col">Bedrijf</th>
+                                                        <th scope="col">Toegevoegd op</th>
+                                                        <th scope="col">Start datum</th>
+                                                        <th scope="col">End datum</th>
+                                                        <th scope="col">Beschrijving</th>
+                                                        <th scope="col">status voorstel</th>
+                                                        <th scope="col">Actie</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                    @foreach($proposals as $proposal)
+                                                        <tr>
+                                                            <td><a href="{{ url('/dashboard/company/' . $proposal->company->id) }}">{{ $proposal->company->name }}</a></td>
+                                                            <td>{{ $proposal->created_at }}</td>
+                                                            <td>{{ $proposal->start_period }}</td>
+                                                            <td>{{ $proposal->end_period }}</td>
+                                                            <td>{{ \Illuminate\Support\Str::words($proposal->description, 10) }}</td>
+                                                            @if ($proposal->visibility)
+                                                                <td><span class="status-p bg-success">{{ $proposal->status }}</span></td>
+                                                            @else
+                                                                <td><span class="status-p bg-primary">{{ $proposal->status }}</span></td>
+                                                            @endif
+                                                            <td>
+                                                                <ul class="d-flex justify-content-center">
+                                                                    <li><a href="{{ url('/dashboard/company/proposal/' . $proposal->id) }}">info</a></li>
+                                                                </ul>
+                                                            </td>
+                                                        </tr>
+                                                    @endforeach
+                                                    </tbody>
+                                                </table>
+                                                <div class="p-3 pull-right">
+                                                    {{ $proposals->links() }}
+                                                </div>
+                                            @else
+                                                <p>Geen resultaten gevonden die {{ $term }} bevatte.</p>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Progress Table end -->
             @endcan
             <!-- Progress Table end -->
                 <!-- footer area start-->
