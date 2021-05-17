37aa084dd568d869b94690da790d1e91f99be24a
Xevro <29582386+Xevro@users.noreply.github.com>
2021-05-18 01:26:15 +0200

    changed the page style and content

--- client/src/views/company/AddProposal.vue
+++ client/src/views/company/AddProposal.vue
@@ -3,8 +3,9 @@
     <Header/>
     <div class="content">
       <div class="title-part">
-        <p>Voeg een taak toe</p>
-        <p>Voeg uw gegevens toe per dag met een beschrijving van uw stage opdracht.</p>
+        <p>Voeg een stage voorstel toe</p>
+        <p>Het stage voorstel zal niet meteen zichtbaar zijn. U zal bericht krijgen van de stagecoördinator wanneer dit gevalideerd is.
+          U kan per stage voorstel 1 student koppelen aan een project of opdracht binnen het bedrijf.</p>
       </div>
       <AddProposalForm :buttonText="'Voeg voorstel toe'"/>
     </div>
@@ -30,3 +31,45 @@ import AddProposalForm from '@/components/UI/molecules/AddProposalForm.vue'
 export default class AddProposal extends Vue {
 }
 </script>
+
+<style scoped>
+.page {
+  min-height: 80vh;
+  overflow: hidden;
+  display: block;
+  position: relative;
+}
+
+.footer {
+  position: relative;
+  bottom: 0;
+  width: 100%;
+  margin-top: 50px;
+}
+
+.title-part {
+  text-align: left;
+  margin-left: 120px;
+  margin-right: 200px;
+}
+
+.title-part p:first-child {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.title-part p:nth-child(2) {
+  font-size: 1.1rem;
+}
+
+.content {
+  padding-top: 100px;
+}
+
+@media screen and (max-width: 700px) {
+  .title-part {
+    margin-left: 50px;
+    margin-right: 50px;
+  }
+}
+</style>
