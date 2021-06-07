6911412b9acf497c8007757f27f6e52058095c11
Xevro <29582386+Xevro@users.noreply.github.com>
2021-06-07 12:02:48 +0200

    docker wijzigingen aangepast

--- docker/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Run Composer Install
-docker exec 2021wmfs-louisdhont_php-web cp .env.example .env
-docker exec 2021wmfs-louisdhont_php-web composer install
-docker exec 2021wmfs-louisdhont_php-web php artisan key:generate
-done
